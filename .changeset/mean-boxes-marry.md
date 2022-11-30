79ec5c7af6bbdb1d042d03f8d3e710d04e0d3a20
xunserver <1358925129@qq.com>
2022-11-30 20:32:50 +0800

    feat: [pub]新增base配置,使用默认的解析器

---
+++ .changeset/mean-boxes-marry.md
@@ -0,0 +1,5 @@
+---
+'@xunserver/eslint-config': patch
+---
+
+feat: 新增 base 配置,使用默认的解析器
--- package/eslint-config/index.js
+++ package/eslint-config/index.js
@@ -4,6 +4,7 @@ const c = (file) => resolve(__dirname, './config', file);
 module.exports = {
   config: {
     recommended: c('recommended'),
+    base: c('base'),
     vue3: c('vue3'),
     typescript: c('typescript'),
   },
